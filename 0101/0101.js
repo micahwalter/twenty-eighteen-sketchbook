4289406bf0ab656b90c83743cedfd1a5320d477c
Micah Walter <micahwalter@outlook.com>
2018-01-01 23:54:39 +0000

    fixed little :bug:

--- 0101/0101.js
+++ 0101/0101.js
@@ -1,13 +1,16 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     textAlign(CENTER);
-    background(255,255,0);
     textSize(32);
 }
 
 function draw() {
+
+    background(255,255,0);
+
     text('Hello, 2018!', windowWidth/2, windowHeight/2);
     fill(0, 0, 255);
+
 }
 
 function windowResized() {
